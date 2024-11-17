23bb7b0bcf5b68d0e361b1e033f97a4dc4f8260f
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-18 00:57:24 +0900

    [Setting] 프로젝트 생성 🎉

---
+++ JangbuRich/JangbuRich/JangbuRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  JangbuRichApp.swift
+//  JangbuRich
+//
+//  Created by 변상우 on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct JangbuRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
