104408cf156785727755daa52780a9bd6dde90a8
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-18 01:24:51 +0900

    [Setting] 폰트 Extension 추가

---
+++ JangbuRich/JangbuRich/Global/Extension/Font+.swift
@@ -0,0 +1,45 @@
+//
+//  Font+.swift
+//  JangbuRich
+//
+//  Created by 변상우 on 11/18/24.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // Headline
+    static let headline1: Font = .custom("Pretendard-Bold", size: 28)
+    static let headline2: Font = .custom("Pretendard-Bold", size: 20)
+    static let headline3: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let headline4: Font = .custom("Pretendard-SemiBold", size: 18)
+    static let headline5: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let headline6: Font = .custom("Pretendard-Bold", size: 22)
+    static let headline7: Font = .custom("Pretendard-SemiBold", size: 22)
+    
+    // Body
+    static let body1: Font = .custom("Pretendard-SemiBold", size: 15)
+    static let body2: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let body3: Font = .custom("Pretendard-Regular", size: 12)
+    static let body4: Font = .custom("Pretendard-Regular", size: 16)
+    static let body5: Font = .custom("Pretendard-SemiBold", size: 12)
+    static let body6: Font = .custom("Pretendard-Bold", size: 18)
+    static let body7: Font = .custom("Pretendard-Medium", size: 18)
+    static let body8: Font = .custom("Pretendard-Medium", size: 22)
+    
+    // Detail
+    static let detail1: Font = .custom("Pretendard-Medium", size: 14)
+    static let detail2: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let detail3: Font = .custom("Pretendard-Medium", size: 10)
+    static let detail4: Font = .custom("Pretendard-Regular", size: 14)
+    static let detail5: Font = .custom("Pretendard-Regular", size: 13)
+    static let detail6: Font = .custom("Pretendard-Medium", size: 13)
+    
+    // Label
+    static let label1: Font = .custom("Pretendard-Medium", size: 16)
+    static let label2: Font = .custom("Pretendard-Medium", size: 14)
+    static let label3: Font = .custom("Pretendard-Medium", size: 12)
+    static let label4: Font = .custom("Pretendard-Regular", size: 14)
+    
+}
