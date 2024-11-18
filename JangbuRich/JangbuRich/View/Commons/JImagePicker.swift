7e00eb75cfa7b03938e998a0720d6e581885883c
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-18 21:24:29 +0900

    [Feat] ImagePicker 커스텀 구현

---
+++ JangbuRich/JangbuRich/View/Commons/JImagePicker.swift
@@ -0,0 +1,45 @@
+//
+//  JImagePicker.swift
+//  JangbuRich
+//
+//  Created by 변상우 on 11/18/24.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct JImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: JImagePicker
+        
+        init(_ parent: JImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            guard let provider = results.first?.itemProvider, provider.canLoadObject(ofClass: UIImage.self) else { return }
+            provider.loadObject(ofClass: UIImage.self) { image, _ in
+                DispatchQueue.main.async {
+                    self.parent.selectedImage = image as? UIImage
+                }
+            }
+        }
+    }
+}
