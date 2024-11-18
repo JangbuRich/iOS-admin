787014389fb04a8f557504533c9fc8ed98947afd
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-19 04:04:23 +0900

    [Feat] 메뉴 Model 구조체 생성

---
+++ JangbuRich/JangbuRich/Model/MenuItem.swift
@@ -0,0 +1,15 @@
+//
+//  MenuItem.swift
+//  JangbuRich
+//
+//  Created by 변상우 on 11/18/24.
+//
+
+import SwiftUI
+
+struct MenuItem {
+    var image: Image
+    var title: String
+    var subtitle: String
+    var price: String
+}
