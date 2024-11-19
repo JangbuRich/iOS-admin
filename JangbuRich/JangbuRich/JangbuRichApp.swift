bc06e7ad63c39af9c8829e1995f4ff7aa0edfa08
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-19 15:14:19 +0900

    [Feat] 카카오 초기 설정 완료

--- JangbuRich/JangbuRich/JangbuRichApp.swift
+++ JangbuRich/JangbuRich/JangbuRichApp.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+import KakaoSDKAuth
+import KakaoSDKUser
+import KakaoSDKCommon
+
 @main
 struct JangbuRichApp: App {
     
     @ObservedObject private var navigationPathManager = NavigationPathManager()
     
+    init() {
+        KakaoSDK.initSDK(appKey: "YOUR_NATIVE_APP_KEY")
+    }
+    
     var body: some Scene {
         WindowGroup {
             LaunchView()
                 .environmentObject(navigationPathManager)
+                .onOpenURL { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
---
+++ JangbuRich/JangbuRich/Store/AuthStore.swift
@@ -0,0 +1,14 @@
+//
+//  AuthStore.swift
+//  JangbuRich
+//
+//  Created by 변상우 on 11/19/24.
+//
+
+import SwiftUI
+
+class AuthStore: ObservableObject {
+    
+
+    
+}
