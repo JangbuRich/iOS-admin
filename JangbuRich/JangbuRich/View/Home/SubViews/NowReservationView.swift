d09ae8927ce6b05f2829f766cd478ac8141a879b
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-23 18:48:49 +0900

    [Feat] 홈 - 진행 중인 예약 그룹 Subview 구현

---
+++ JangbuRich/JangbuRich/View/Home/SubViews/NowReservationView.swift
@@ -0,0 +1,76 @@
+//
+//  NowReservationView.swift
+//  JangbuRich
+//
+//  Created by 변상우 on 11/20/24.
+//
+
+import SwiftUI
+
+struct NowReservationView: View {
+    var reservation: NowReservation
+    
+    var body: some View {
+        VStack {
+            VStack {
+                HStack {
+                    Image(.aiHelperButton)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: scaledHeight(24))
+                    
+                    Button {
+                        print("이동")
+                    } label: {
+                        HStack {
+                            Text(reservation.name)
+                                .font(.detail2)
+                                .foregroundStyle(.jgray20)
+                            
+                            Image(.iconBack)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: scaledHeight(20))
+                        }
+                    }
+
+                    Spacer()
+                }
+                
+                Divider()
+                    .frame(height: scaledHeight(1))
+                    .background(.jgray90)
+                    .padding(.vertical, scaledHeight(20))
+                
+                VStack(spacing: scaledHeight(5)) {
+                    HStack {
+                        Text("인원")
+                            .font(.label3)
+                            .foregroundStyle(.jgray60)
+                        
+                        Spacer()
+                        
+                        Text(reservation.numberOfPeople)
+                            .font(.body5)
+                            .foregroundStyle(.jgray30)
+                    }
+                    
+                    HStack {
+                        Text("날짜")
+                            .font(.label3)
+                            .foregroundStyle(.jgray60)
+                        
+                        Spacer()
+                        
+                        Text(reservation.reservationDate)
+                            .font(.body5)
+                            .foregroundStyle(.jgray30)
+                    }
+                }
+            }
+            .padding(scaledWidth(20))
+        }
+        .background(.jgray100)
+        .cornerRadius(scaledWidth(10))
+    }
+}
