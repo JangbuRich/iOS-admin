d42a0753a11ad2779f4bced3ea35fe10d49864a9
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-23 21:33:27 +0900

    [Feat] 전체 결제 내역 리스트뷰 UI 구현

---
+++ JangbuRich/JangbuRich/View/JangbuManage/JangbuHistoryListView.swift
@@ -0,0 +1,79 @@
+//
+//  JangbuHistoryListView.swift
+//  JangbuRich
+//
+//  Created by 변상우 on 11/23/24.
+//
+
+import SwiftUI
+
+struct JangbuHistoryListView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    let historyList: [JangbuHistory]
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        HStack(spacing: scaledWidth(10)) {
+                            Image(.iconBackBlack)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: scaledHeight(24))
+                            
+                            Text("전체 결제 내역")
+                                .font(.headline6)
+                                .foregroundStyle(.jgray20)
+                        }
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top, scaledHeight(80))
+                .padding(.bottom, scaledHeight(20))
+                
+                VStack {
+                    HStack {
+                        Text("#")
+                            .frame(maxWidth: scaledWidth(49), alignment: .leading)
+                        
+                        Text("날짜")
+                            .frame(maxWidth: scaledWidth(115), alignment: .leading)
+                        
+                        Text("그룹원")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Text("결제 금액")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .font(.body2)
+                    .foregroundStyle(.jgray30)
+                    
+                    Rectangle()
+                        .fill(.jgray80)
+                        .frame(height: scaledHeight(1))
+                        .padding(.vertical, scaledHeight(15))
+                    
+                    VStack(spacing: scaledHeight(20)) {
+                        ForEach(historyList.prefix(10), id: \.jangbuNumber) { history in
+                            JangbuHistoryView(jangbHistory: history)
+                        }
+                    }
+                }
+                .padding(scaledWidth(20))
+                .background(.jgray100)
+                .cornerRadius(scaledWidth(10))
+            }
+            .padding(.leading, scaledWidth(44))
+            .padding(.trailing, scaledWidth(45))
+        }
+        .navigationBarBackButtonHidden(true)
+        .scrollIndicators(.hidden)
+        .background(.jgray95)
+    }
+}
