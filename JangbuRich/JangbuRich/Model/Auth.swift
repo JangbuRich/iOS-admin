ad482179b8fba43a239311bb9424d6e84dc606b5
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-24 04:47:45 +0900

    [Feat] 회원가입 DTO 생성

--- JangbuRich/JangbuRich/Model/Auth.swift
+++ JangbuRich/JangbuRich/Model/Auth.swift
@@ -12,4 +12,41 @@ struct Token: Codable {
     let accessTokenExpires: Int
     let refreshToken: String
     let refreshTokenExpires: Int
+    let alreadyExists: Bool
+}
+
+struct CreateUserRequestDTO: Codable {
+    var image: Data
+    var store: OnboardingUser
+    var menuImages: [Data]
+}
+
+struct OnboardingUser: Codable {
+    var storeName: String
+    var phoneNumber: String
+    var businessName: String
+    var businessRegistrationNumber: String
+    var openingDate: String
+    var agreeMarketing: Bool
+    var agreeAdvertise: Bool
+    var introduction: String
+    var category: String
+    var latitude: Double
+    var longitude: Double
+    var address: String
+    var location: String
+    var dayOfWeek: [String]
+    var openTime: String
+    var closeTime: String
+    var menuCreateRequestDTOS: [MenuCreateRequestDTO]
+    var minPrepayment: Int
+    var prepaymentDuration: Int
+    var reservationAvailable: Bool
+    var maxReservation: Int
+}
+
+struct MenuCreateRequestDTO: Codable {
+    var name: String
+    var description: String
+    var price: Int
 }
