0dc5f35d01eadccf6b40e4afda01fa14dd3e50e4
Sangwoo Byeon <tkddn0518@naver.com>
2024-11-28 01:33:33 +0900

    [Feat] 날짜 형식 formatter 전역 함수 구현

---
+++ JangbuRich/JangbuRich/View/Commons/Functions.swift
@@ -0,0 +1,35 @@
+//
+//  Functions.swift
+//  JangbuRich
+//
+//  Created by 변상우 on 11/28/24.
+//
+
+import Foundation
+
+public func formatDate(_ dateString: String) -> String {
+    let trimmedDateString = trimMilliseconds(dateString)
+
+    let inputFormatter = DateFormatter()
+    inputFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+    inputFormatter.locale = Locale(identifier: "en_US_POSIX")
+    inputFormatter.timeZone = TimeZone(abbreviation: "UTC")
+
+    let outputFormatter = DateFormatter()
+    outputFormatter.dateFormat = "yyyy.MM.dd"
+
+    if let date = inputFormatter.date(from: trimmedDateString) {
+        return outputFormatter.string(from: date)
+    } else {
+        return "Invalid Date"
+    }
+}
+
+public func trimMilliseconds(_ dateString: String) -> String {
+    if let range = dateString.range(of: "\\.\\d+", options: .regularExpression) {
+        let milliseconds = dateString[range]
+        let trimmedMilliseconds = String(milliseconds.prefix(4))
+        return dateString.replacingCharacters(in: range, with: trimmedMilliseconds)
+    }
+    return dateString
+}
