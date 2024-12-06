3edeb457583f31469231f04a8738feb94da24fd7
Sangwoo Byeon <tkddn0518@naver.com>
2024-12-06 17:29:50 +0900

    [Feat] 전체 결제 내역 엑셀로 내보내기 API 함수 구현

--- JangbuRich/JangbuRich/Store/JangbuStore.swift
+++ JangbuRich/JangbuRich/Store/JangbuStore.swift
@@ -109,4 +109,87 @@ class JangbuStore: ObservableObject {
             }
         }
     }
+    
+    func getHistoryExcel(period: Int) {
+        let url = Config.baseURL + "store/excel"
+        
+        let params = [
+            "period" : period
+        ]
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(KeychainStore.sharedKeychain.getAccessToken() ?? "")"
+        ]
+    
+        AF.download(url,
+                   method: .get,
+                   parameters: params,
+                   headers: headers
+        ).validate().response { response in
+            if let tempURL = response.fileURL {
+                var suggestedFileName = "TestExcel.xlsx"
+                if let contentDisposition = response.response?.allHeaderFields["Content-Disposition"] as? String {
+                    suggestedFileName = self.parseFilename(from: contentDisposition) ?? suggestedFileName
+                }
+                if let contentType = response.response?.allHeaderFields["Content-Type"] as? String {
+                    suggestedFileName = self.adjustFileExtension(for: suggestedFileName, contentType: contentType)
+                }
+                if let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                    let destinationURL = directory.appendingPathComponent(suggestedFileName)
+                    do {
+                        try FileManager.default.moveItem(at: tempURL, to: destinationURL)
+                        self.saveToFilesApp(url: destinationURL)
+                    } catch {
+                        print("Failed to move file: \(error.localizedDescription)")
+                    }
+                } else {
+                    print("Unable to find document directory")
+                }
+            } else if let error = response.error {
+                print("Failed to download file: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func parseFilename(from contentDisposition: String) -> String? {
+        let pattern = "filename=\"(.*?)\""
+        if let regex = try? NSRegularExpression(pattern: pattern, options: []),
+           let match = regex.firstMatch(in: contentDisposition, options: [], range: NSRange(location: 0, length: contentDisposition.count)) {
+            if let range = Range(match.range(at: 1), in: contentDisposition) {
+                return String(contentDisposition[range])
+            }
+        }
+        return nil
+    }
+    
+    func adjustFileExtension(for fileName: String, contentType: String) -> String {
+        let fileExtension: String
+        switch contentType {
+        case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            fileExtension = ".xlsx"
+        case "application/vnd.ms-excel":
+            fileExtension = ".xls"
+        case "application/octet-stream":
+            if fileName.hasSuffix(".xlsx") || fileName.hasSuffix(".xls") {
+                fileExtension = ""
+            } else {
+                fileExtension = ".xlsx"  // 기본적으로 .xlsx로 저장
+            }
+        default:
+            fileExtension = ""
+        }
+        
+        if fileName.hasSuffix(fileExtension) {
+            return fileName
+        } else {
+            return fileName + fileExtension
+        }
+    }
+    
+    func saveToFilesApp(url: URL) {
+        let documentPicker = UIDocumentPickerViewController(forExporting: [url])
+        if let rootVC = UIApplication.shared.windows.first?.rootViewController {
+            rootVC.present(documentPicker, animated: true, completion: nil)
+        }
+    }
 }
