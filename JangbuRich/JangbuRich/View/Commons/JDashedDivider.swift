29539d4655377639b7bd871a31f1b9a8595d4855
Sangwoo Byeon <tkddn0518@naver.com>
2024-12-07 03:54:54 +0900

    [Fix] 커스텀 대쉬 Divider 수정

--- JangbuRich/JangbuRich/View/Commons/JDashedDivider.swift
+++ JangbuRich/JangbuRich/View/Commons/JDashedDivider.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 struct JDashedDivider: View {
-    var color: Color = .gray
-    var lineWidth: CGFloat = 1
-    var dashPattern: [CGFloat] = [5, 5] // 점선 패턴 [길이, 간격]
+    var color: Color
+    var lineWidth: CGFloat
+    var dashPattern: [CGFloat] // 점선 패턴 [길이, 간격]
     
     var body: some View {
-        Rectangle()
-            .frame(height: lineWidth)
-            .foregroundStyle(.clear)
-            .overlay(
-                Rectangle()
-                    .stroke(style: StrokeStyle(lineWidth: lineWidth, dash: dashPattern))
-                    .foregroundStyle(color)
-            )
-            .frame(maxWidth: .infinity)
+        GeometryReader { geometry in
+            Path { path in
+                path.move(to: CGPoint(x: 0, y: geometry.size.height / 2))
+                path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height / 2))
+            }
+            .stroke(style: StrokeStyle(lineWidth: lineWidth, dash: dashPattern))
+            .foregroundStyle(color)
+        }
+        .frame(height: lineWidth)
     }
 }
